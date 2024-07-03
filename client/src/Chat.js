8c89f43ee9597da440ba4b17a501c6860172d622
Mudambi Himakiran <Handsomehunk.hk@gmail.com>
2024-07-03 20:56:22 +0530

    chat Updated

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,44 +1,12 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
-import micIcon from "./google_mic_icon.png";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
-  const [isRecording, setIsRecording] = useState(false);
-  const [audioURL, setAudioURL] = useState("");
-  const mediaRecorderRef = useRef(null);
-  const audioChunksRef = useRef([]);
-
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaRecorderRef.current = new MediaRecorder(stream);
-      audioChunksRef.current = [];
-
-      mediaRecorderRef.current.ondataavailable = (event) => {
-        audioChunksRef.current.push(event.data);
-      };
-
-      mediaRecorderRef.current.start();
-      setIsRecording(true);
-    } catch (err) {
-      console.error("Error accessing the microphone", err);
-    }
-  };
-
-  const stopRecording = () => {
-    mediaRecorderRef.current.stop();
-    mediaRecorderRef.current.onstop = () => {
-      const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
-      const audioUrl = URL.createObjectURL(audioBlob);
-      setAudioURL(audioUrl);
-      setIsRecording(false);
-    };
-  };
 
   const sendMessage = async () => {
-    if (currentMessage.trim() !== "") {
+    if (currentMessage !== "") {
       const messageData = {
         room: room,
         author: username,
@@ -49,16 +17,11 @@ function Chat({ socket, username, room }) {
           new Date(Date.now()).getMinutes(),
       };
 
-      try {
-        await socket.emit("send_message", messageData);
-        setMessageList((list) => [...list, messageData]);
-        setCurrentMessage("");
-      } catch (error) {
-        console.error("Error sending message:", error);
-      }
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
-
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
@@ -73,62 +36,179 @@ function Chat({ socket, username, room }) {
       </div>
       <div className="chat-body">
         <ScrollToBottom className="message-container">
-          {messageList.map((messageContent) => (
-            <div
-              className="message"
-              id={username === messageContent.author ? "you" : "other"}
-              key={messageContent.time}
-            >
-              <div>
-                <div className="message-content">
-                  <p>{messageContent.message}</p>
-                </div>
-                <div className="message-meta">
-                  <p id="time">{messageContent.time}</p>
-                  <p id="author">{messageContent.author}</p>
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </ScrollToBottom>
       </div>
-
       <div className="chat-footer">
         <input
           type="text"
           value={currentMessage}
-          onChange={(event) => setCurrentMessage(event.target.value)}
-          onKeyPress={(event) => event.key === "Enter" && sendMessage()}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
         />
-
-        {isRecording ? (
-          <img
-            src={micIcon}
-            alt="Microphone"
-            className="mic-icon"
-            onClick={stopRecording}
-          />
-        ) : currentMessage.trim() !== "" ? (
-          <button className="send-button" onClick={sendMessage}>
-            &#9658;
-          </button>
-        ) : (
-          <img
-            src={micIcon}
-            alt="Microphone"
-            className="mic-icon"
-            onClick={startRecording}
-          />
-        )}
+        <button onClick={sendMessage}>&#9658;</button>
       </div>
-
-      {audioURL && (
-        <div>
-          <audio src={audioURL} controls />
-        </div>
-      )}
     </div>
   );
 }
 
 export default Chat;
+
+
+
+// import React, { useEffect, useState, useRef } from "react";
+// import ScrollToBottom from "react-scroll-to-bottom";
+// import micIcon from "./google_mic_icon.png";
+
+// function Chat({ socket, username, room }) {
+//   const [currentMessage, setCurrentMessage] = useState("");
+//   const [messageList, setMessageList] = useState([]);
+//   const [isRecording, setIsRecording] = useState(false);
+//   const [audioURL, setAudioURL] = useState("");
+//   const mediaRecorderRef = useRef(null);
+//   const audioChunksRef = useRef([]);
+
+//   const startRecording = async () => {
+//     try {
+//       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+//       mediaRecorderRef.current = new MediaRecorder(stream);
+//       audioChunksRef.current = [];
+
+//       mediaRecorderRef.current.ondataavailable = (event) => {
+//         audioChunksRef.current.push(event.data);
+//       };
+
+//       mediaRecorderRef.current.start();
+//       setIsRecording(true);
+//     } catch (err) {
+//       console.error("Error accessing the microphone", err);
+//     }
+//   };
+
+//   const stopRecording = () => {
+//     mediaRecorderRef.current.stop();
+//     mediaRecorderRef.current.onstop = () => {
+//       const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
+//       const audioUrl = URL.createObjectURL(audioBlob);
+//       setAudioURL(audioUrl);
+//       setIsRecording(false);
+//     };
+//   };
+
+//   const sendMessage = async () => {
+//     if (currentMessage.trim() !== "") {
+//       const messageData = {
+//         room: room,
+//         author: username,
+//         message: currentMessage,
+//         time:
+//           new Date(Date.now()).getHours() +
+//           ":" +
+//           new Date(Date.now()).getMinutes(),
+//       };
+
+//       try {
+//         await socket.emit("send_message", messageData);
+//         setMessageList((list) => [...list, messageData]);
+//         setCurrentMessage("");
+//       } catch (error) {
+//         console.error("Error sending message:", error);
+//       }
+//     }
+//   };
+
+
+//   useEffect(() => {
+//     socket.on("receive_message", (data) => {
+//       setMessageList((list) => [...list, data]);
+//     });
+//   }, [socket]);
+
+//   return (
+//     <div className="chat-window">
+//       <div className="chat-header">
+//         <p>Live Chat</p>
+//       </div>
+//       <div className="chat-body">
+//         <ScrollToBottom className="message-container">
+//           {messageList.map((messageContent) => (
+//             <div
+//               className="message"
+//               id={username === messageContent.author ? "you" : "other"}
+//               key={messageContent.time}
+//             >
+//               <div>
+//                 <div className="message-content">
+//                   <p>{messageContent.message}</p>
+//                 </div>
+//                 <div className="message-meta">
+//                   <p id="time">{messageContent.time}</p>
+//                   <p id="author">{messageContent.author}</p>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </ScrollToBottom>
+//       </div>
+
+//       <div className="chat-footer">
+//         <input
+//           type="text"
+//           value={currentMessage}
+//           onChange={(event) => setCurrentMessage(event.target.value)}
+//           onKeyPress={(event) => event.key === "Enter" && sendMessage()}
+//         />
+
+//         {isRecording ? (
+//           <img
+//             src={micIcon}
+//             alt="Microphone"
+//             className="mic-icon"
+//             onClick={stopRecording}
+//           />
+//         ) : currentMessage.trim() !== "" ? (
+//           <button className="send-button" onClick={sendMessage}>
+//             &#9658;
+//           </button>
+//         ) : (
+//           <img
+//             src={micIcon}
+//             alt="Microphone"
+//             className="mic-icon"
+//             onClick={startRecording}
+//           />
+//         )}
+//       </div>
+
+//       {audioURL && (
+//         <div>
+//           <audio src={audioURL} controls />
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Chat;
