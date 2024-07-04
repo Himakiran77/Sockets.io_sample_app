ca52e85deb318f098fa7080876036ce8181022e3
Mudambi Himakiran <Handsomehunk.hk@gmail.com>
2024-07-04 14:41:23 +0530

    index Updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://friends-prnj.onrender.com",
     methods: ["GET", "POST"],
   },
 });
